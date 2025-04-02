b11c90f1c42d1380163fb61ab91d7e8b06e2ca3f
breadsnicks <aheraldo09@gmail.com>
2025-04-01 21:39:05 -0400

    add image

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QWidget
+from PyQt5.uic import loadUi
+
 import pandas as pd
 import numpy as np
 
+# Read the ROC data file
 roc_data = pd.read_csv('roc_train_data.csv')
+fpr = roc_data["False Positive Rate (FPR)"].values
+tpr = roc_data["True Positive Rate (TPR)"].values
+threshold = roc_data["Thresholds"].values
 
-class MainWindow(QMainWindow):
+class MainWindow(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Accelerometer Classification")
-        self.setGeometry(700, 300, 800, 600)
-        self.setWindowIcon(QIcon("coatofarms.png"))
-        self.center_window()
+        loadUi("gui.ui", self)
+        self.browse.clicked.connect(self.browseFiles)
 
-    def center_window(self):
-        frame_geometry = self.frameGeometry()  # Get the window's size
-        screen_center = QDesktopWidget().availableGeometry().center()  # Get the screen's center
-        frame_geometry.moveCenter(screen_center)  # Move the window's center
-        self.move(frame_geometry.topLeft())  # Set the new top-left position
+    def browseFiles(self):
+        file_path, _ =QFileDialog.getOpenFileName(self, 'Open File', '', '(*.csv)')
+        if file_path:
+            self.processCSV(file_path)
 
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    def processCSV(self, file_path):
+        data = pd.read_csv(file_path)
+        time = data.iloc[:,0].values
+        x = data.iloc[:,1].values
+        y = data.iloc[:,2].values
+        z = data.iloc[:,3].values
+        absolute_acceleration = data.iloc[:,4].values
 
-if __name__ == "__main__":
-    main()
+app = QApplication(sys.argv)
+main = MainWindow()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(main)
+widget.setFixedWidth(400)
+widget.setFixedHeight(300)
+widget.show()
+sys.exit(app.exec_())
+
